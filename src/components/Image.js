2d1f03d8f689ed693091d5026a0beab9f95291ad
Saliou1920 <diopsaliou45@gmail.com>
2021-09-12 16:09:08 -0300

    Designing card

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,18 +2,16 @@ import React from 'react'
 import '../assets/css/Image.css';
 export default function Image({image}) {
     return (
-        <div className="test">
-            <div className="img">
+        
+        <section className="container">
             <div className="card">
                 <h2>{image.title}</h2>
-                <img src={image.url} alt="" width="300px"/>
-                <div className="container">
-                    <h4><b>John Doe</b></h4> 
-                    <p>{image.explanation}</p> 
+                <div className="">
+                    <img src={image.url} alt="" className="card-image"/>
                 </div>
-            </div>
-        </div>
-        </div>
+                <p className="card-text">{image.explanation}</p> 
+            </div> 
+        </section>
         
     )
 }
